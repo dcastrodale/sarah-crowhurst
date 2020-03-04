7bf88c7adb15a3ea5352d4232e52f612597a30a6
David Castrodale <dcastrodale@gmail.com>
2020-03-04 13:21:24 +0000

    Move window check into componentDidMount

--- src/components/CTAs/GoBack/index.js
+++ src/components/CTAs/GoBack/index.js
@@ -1,21 +1,41 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './goback.scss';
 
 const handleGoBack = event => {
   event.preventDefault();
   window.history.back();
 }
-export default () => {
-  return (window && window.history) ?
-    (
-      <div className="go-back cta cta--dark">
-        <a
-          href="#"
-          onClick={event => handleGoBack(event)}
-        >
-          &lt; Go back
+
+// We need to check for the window object in componentDidMount so that it can render properly on the server
+// as there is no window object on the server at build
+export default class GoBack extends Component {
+  constructor() {
+    super();
+    this.state = {
+      historyAvailable: false
+    }
+  }
+
+  componentDidMount() {
+    if (window && window.history) {
+      this.setState({
+        historyAvailable: true
+      })
+    }
+  }
+
+  render() {
+    return this.state.historyAvailable ?
+      (
+        <div className="go-back cta cta--dark">
+          <a
+            href="#"
+            onClick={event => handleGoBack(event)}
+          >
+            &lt; Go back
         </a>
-      </div>
-    ) :
-    null;
+        </div>
+      ) :
+      null;
+  }
 }
